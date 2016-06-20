0bcb6b79af61dc096ac4dad83789fb764c386a4e
Jared Hoberock <jaredhoberock@gmail.com>
2016-06-20 16:05:06 -0500

    Optimize copy algorithms

--- algorithm/copy.hpp
+++ algorithm/copy.hpp
@@ -4,20 +4,32 @@
 #include <agency/execution_agent.hpp>
 #include <agency/experimental/span.hpp>
 #include <agency/experimental/chunk.hpp>
+#include <agency/experimental/zip.hpp>
 #include "../algorithm.hpp"
+#include "for_loop.hpp"
 #include "../bound.hpp"
 
 template<std::size_t bound, class Range1, class Range2>
 __device__
 void sequential_bounded_copy(Range1&& in, Range2&& out)
 {
-  for(std::size_t i = 0; i < bound; ++i)
+  bounded_for_loop<bound>(in.size(), [&](int i)
   {
-    if(i < in.size())
-    {
-      out[i] = in[i];
-    }
-  }
+    out[i] = in[i];
+  });
+}
+
+
+namespace detail
+{
+
+template<class Tuple>
+__AGENCY_ANNOTATION
+void assign_first_to_second(const Tuple& t)
+{
+  agency::detail::get<1>(t) = agency::detail::get<0>(t);
+}
+
 }
 
 
@@ -41,31 +53,31 @@ void bounded_copy(agency::experimental::static_concurrent_agent<group_size, grai
   self.wait();
 }
 
-// XXX eliminate this
+// XXX eliminate this and replace the above's implementation with this one
 template<std::size_t group_size, std::size_t grain_size, class Range1, class Range2>
 __device__
-void bounded_copy(const Range1& in, Range2&& out)
+void bounded_copy(int tid, const Range1& in, Range2&& out)
 {
-  if(group_size * grain_size <= in.size())
-  {
-    for(size_t i = 0; i < grain_size; ++i)
-    {
-      int offset = threadIdx.x + i * group_size;
+  auto in_and_out = zip(in, out);
+  auto view_of_this_agents_range = stride(drop(in_and_out, tid), group_size);
 
-      out[offset] = in[offset];
-    }
+  // XXX if we were able to express this as a cyclic_split instead of a stride(),
+  //     we could probably compute the size of the agent's range without a divide
+  //     because we'd have more information when computing the size()
+
+  if(in_and_out.size() == group_size * grain_size)
+  {
+    static_for_loop<grain_size>([&](int i)
+    {
+      detail::assign_first_to_second(view_of_this_agents_range[i]);
+    });
   }
   else
   {
-    for(size_t i = 0; i < grain_size; ++i)
+    bounded_for_loop<grain_size>(view_of_this_agents_range.size(), [&](int i)
     {
-      int offset = threadIdx.x + i * group_size;
-
-      if(offset < in.size())
-      {
-        out[offset] = in[offset];
-      }
-    }
+      detail::assign_first_to_second(view_of_this_agents_range[i]);
+    });
   }
 
   __syncthreads();
